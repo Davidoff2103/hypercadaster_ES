52611d338b41c346954d48fe52dedfffc1914acf
David Blanco Bautista <73123058+Davidoff2103@users.noreply.github.com>
2025-07-18 09:28:46 +0200

    Add docstrings and comments to functions.py

--- hypercadaster_ES/functions.py
+++ hypercadaster_ES/functions.py
@@ -5,6 +5,25 @@ from hypercadaster_ES import utils
 from hypercadaster_ES import downloaders
 from zipfile import ZipFile, BadZipFile
 
+"""
+Download cadaster data for specified municipalities.
+
+Args:
+    wd (str): Working directory path.
+    province_codes (list, optional): List of province codes.
+    ine_codes (list, optional): List of INE municipality codes.
+    cadaster_codes (list, optional): List of cadaster codes.
+    neighborhood_layer (bool, optional): Whether to include neighborhood data.
+    postal_code_layer (bool, optional): Whether to include postal code data.
+    census_layer (bool, optional): Whether to include census tract data.
+    elevation_layer (bool, optional): Whether to include elevation data.
+    open_data_layers (bool, optional): Whether to include Open Data Barcelona layers.
+    force (bool, optional): Force re-download of files.
+
+Returns:
+    gpd.GeoDataFrame: Merged geospatial data.
+"""
+
 def download(wd, province_codes=None, ine_codes=None, cadaster_codes=None,
              neighborhood_layer=True, postal_code_layer=True, census_layer=True,
              elevation_layer=True, open_data_layers=True, force=False):
@@ -24,12 +43,11 @@ def download(wd, province_codes=None, ine_codes=None, cadaster_codes=None,
         raise ValueError("Municipality INE codes (ine_codes) or cadaster codes (cadaster_codes) should be provided, not both!")
     elif ine_codes is None and cadaster_codes is None:
         if province_codes is None:
-            raise ValueError("One of the arguments must be provided: municipality INE codes (ine_codes) or cadaster codes "
-                             "(cadaster_codes), or province codes (province_codes)")
+            raise ValueError("One of the arguments must be provided: municipality INE codes (ine_codes) or cadaster codes \"\n                             "\n                             "(cadaster_codes), or province codes (province_codes)")
         else:
             municipalities = utils.list_municipalities(
                 province_codes=province_codes, echo=False)
-            cadaster_codes = [item['name'].split("-")[0] for item in municipalities]
+            cadaster_codes = [item['name'].split("-") [0] for item in municipalities]
             ine_codes = utils.cadaster_to_ine_codes(utils.cadaster_dir_(wd), cadaster_codes)
     elif ine_codes is None:
         ine_codes = utils.cadaster_to_ine_codes(utils.cadaster_dir_(wd), cadaster_codes)
@@ -43,12 +61,10 @@ def download(wd, province_codes=None, ine_codes=None, cadaster_codes=None,
     # Districts and neighborhoods definition are only available for the city of Barcelona
     if '08019' in ine_codes and neighborhood_layer:
         downloaders.download_file(dir=utils.districts_dir_(wd),
-                                  url="https://opendata-ajuntament.barcelona.cat/data/dataset/808daafa-d9ce-48c0-"
-                                      "925a-fa5afdb1ed41/resource/576bc645-9481-4bc4-b8bf-f5972c20df3f/download",
+                                  url="https://opendata-ajuntament.barcelona.cat/data/dataset/808daafa-d9ce-48c0-\"\n                                      "\n                                      "925a-fa5afdb1ed41/resource/576bc645-9481-4bc4-b8bf-f5972c20df3f/download",
                                   file="districts.csv")
         downloaders.download_file(dir=utils.neighborhoods_dir_(wd),
-                                  url="https://opendata-ajuntament.barcelona.cat/data/dataset/808daafa-d9ce-48c0-"
-                                      "925a-fa5afdb1ed41/resource/b21fa550-56ea-4f4c-9adc-b8009381896e/download",
+                                  url="https://opendata-ajuntament.barcelona.cat/data/dataset/808daafa-d9ce-48c0-\"\n                                      "\n                                      "925a-fa5afdb1ed41/resource/b21fa550-56ea-4f4c-9adc-b8009381896e/download",
                                   file="neighbourhoods.csv")
     # Postal codes
     if postal_code_layer or elevation_layer:
@@ -65,26 +81,23 @@ def download(wd, province_codes=None, ine_codes=None, cadaster_codes=None,
     # OpenDataBarcelona
     if '08019' in ine_codes and open_data_layers:
         downloaders.download_file(dir=utils.open_data_dir_(wd),
-                                  url="https://opendata-ajuntament.barcelona.cat/data/dataset/3dc277bf-ff89-4b49-8f"
-                                      "29-48a1122bb813/resource/2e123ea9-1819-46cf-a545-be61151fa97d/download",
+                                  url="https://opendata-ajuntament.barcelona.cat/data/dataset/3dc277bf-ff89-4b49-8f\"\n                                      "\n                                      "29-48a1122bb813/resource/2e123ea9-1819-46cf-a545-be61151fa97d/download",
                                   file="barcelona_establishments.csv")
         downloaders.download_file(dir=utils.open_data_dir_(wd),
-                                  url="https://opendata-ajuntament.barcelona.cat/data/dataset/fe177673-0f83-42e7-b3"
-                                      "5a-ddea901be8bc/resource/99764d55-b1be-4281-b822-4277442cc721/download/22093"
-                                      "0_censcomercialbcn_opendata_2022_v10_mod.csv",
+                                  url="https://opendata-ajuntament.barcelona.cat/data/dataset/fe177673-0f83-42e7-b3\"\n                                      "\n                                      "5a-ddea901be8bc/resource/99764d55-b1be-4281-b822-4277442cc721/download/22093\"\n                                      "\n                                      "0_censcomercialbcn_opendata_2022_v10_mod.csv",
                                   file="barcelona_ground_premises.csv")
         downloaders.download_file(dir=utils.open_data_dir_(wd),
-                                  url="https://opendata-ajuntament.barcelona.cat/data/dataset/6b5cfa7b-1d8d-45f0-990a-"
-                                      "d1844d43ffd1/resource/26c6be33-44f5-4596-8a29-7ac152546ca7/download",
+                                  url="https://opendata-ajuntament.barcelona.cat/data/dataset/6b5cfa7b-1d8d-45f0-990a-\"\n                                      "\n                                      "d1844d43ffd1/resource/26c6be33-44f5-4596-8a29-7ac152546ca7/download",
                                   file="barcelona_carrerer.zip")
         try:
             with ZipFile(f"{utils.open_data_dir_(wd)}/barcelona_carrerer.zip", 'r') as zip:
                 zip.extractall(utils.open_data_dir_(wd))
                 os.rename(f"{utils.open_data_dir_(wd)}/Adreces_elementals.gpkg",
                           f"{utils.open_data_dir_(wd)}/barcelona_carrerer.gpkg")
-                os.remove(f"{utils.open_data_dir_(wd)}/barcelona_carrerer.zip")
+                os.remove(f"{utils.open_data_dir_(wd)}/bar: barcelona_carrerer.zip")
         except BadZipFile:
             os.remove(f"{utils.open_data_dir_(wd)}/barcelona_carrerer.zip")
+
 
 def merge(wd, province_codes=None, ine_codes=None, cadaster_codes=None,
           neighborhood_layer=True, postal_code_layer=True, census_layer=True, elevations_layer=True,
@@ -98,12 +111,11 @@ def merge(wd, province_codes=None, ine_codes=None, cadaster_codes=None,
     elif ine_codes is None and cadaster_codes is None:
         if province_codes is None:
             raise ValueError(
-                "One of the arguments must be provided: municipality INE codes (ine_codes) or cadaster codes "
-                "(cadaster_codes), or province codes (province_codes)")
+                "One of the arguments must be provided: municipality INE codes (ine_codes) or cadaster codes \"\n                "\n                "(cadaster_codes), or province codes (province_codes)")
         else:
             municipalities = utils.list_municipalities(
                 province_codes=province_codes, echo=False)
-            cadaster_codes = [item['name'].split("-")[0] for item in municipalities]
+            cadaster_codes = [item['name'].split("-") [0] for item in municipalities]
     elif cadaster_codes is None:
         cadaster_codes = utils.ine_to_cadaster_codes(utils.cadaster_dir_(wd), ine_codes)
 
@@ -123,7 +135,8 @@ def merge(wd, province_codes=None, ine_codes=None, cadaster_codes=None,
         building_parts_plots=building_parts_plots,
         open_data_layers=open_data_layers,
         open_data_layers_dir=utils.open_data_dir_(wd),
-        CAT_files_dir=f"{wd}/{CAT_files_rel_dir}")
+        CAT_files_dir=f"{wd}/{CAT_files_rel_dir}"
+    )
 
     if census_layer:
         gdf = mergers.join_by_census_tracts(
